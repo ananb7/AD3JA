86a56fc45a20dbd975addfe9f5a432f3e55e7c13
ana.nunez <anaym10@gmail.com>
2024-02-07 22:41:16 +0100

    first commit

---
+++ src/main/java/eventos/NbaAd3EventosApplication.java
@@ -0,0 +1,13 @@
+package eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaAd3EventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaAd3EventosApplication.class, args);
+	}
+
+}
